e021551343a9a7313da735d291c8f04bb04d579f
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2021-09-10 01:54:46 +0530

    Links design and js functions done need json data.

--- loadingExamples.js
+++ loadingExamples.js
@@ -1,18 +1,6 @@
 $(document).ready(function () {
-
-    /*
-        Will work in this sequence
-        - Load the JSON data
-        - as soon the data get loaded hide loader
-        - add links in loop
-    */
-
     loadData();
-
 })
-
-
-
 
 // Load the JSON data
 function loadData() {
@@ -21,38 +9,48 @@ function loadData() {
         function (jsonData) {
             let user = jsonData.filter(data => { if (data.maild_id === "ujjwalpandey.aps@gmail.com") return data; })[0]
             console.log('we got the data ', user);
-            //Data loaded in the variable "user"
-            // Now hide the loader
-            console.log("sleeping loader will hide in 1.5 seconds");
+            //Data loaded in the variable "user" Now hide the loader
             setTimeout(() => {
                 hideLoader();
-                console.log("loaded hided, now adding links");
-                //Adding links
-                addingLinks(user);
-            }, 1500);
+                //Adding links choose any one
+                addingLinksAtOnce(user);
+                // addingLinksAtInterval
+            }, 1750);
         }
     );
 }
+
 // Add links in loop
-function addingLinks(userData) {
+function addingLinksAtOnce(userData) {
     userData.WorkExamples.forEach(singleData => {
         console.log(singleData);
         let dataToBeAdded = `
         <div class="links">
             <div>
-                <div class="image">work img</div>
-                <div class="details">jquery added data Example title (as a link)<br />(IsResponsive)</div>
+                <div class="image">
+                    <img src="examplesPreviewImages/${(singleData.imageNameWithExtension === "none") ? "noImageFound.jpg" : (singleData.imageNameWithExtension) ? singleData.imageNameWithExtension : "noImageFound.jpg"}" alt="work preview">
+                </div>
+                <div class="details"><a href="${singleData.exampleLink}">${singleData.title}</a><br /><span>Is Responsive :
+                        <i>${(singleData.responsive) ? "Yes" : "No"}</span></i>
+                </div>
             </div>
         </div>`
         $(".main").append(dataToBeAdded);
     });
     $(".addLinksBeforeMe").hide();
 }
+function addingLinksAtInterval(userData) {
+    //  with it the links will show one by one with a very minute interval of 200ms.
+    /*
+        Code writing
+        --> run a forEach loop on "userData.WorkExamples" and for every data in the loop will call function with the data at a setInterval or setTimeout in the loop,
+        the function called will do the all work of - loading json data -hiding loader - and adding it to the html.
+    */
+}
+
+
 
 // hide the Loading circle
 function hideLoader() {
     $(".loader").toggle();
-    /* setTimeout(() => {
-        $(this).toggle();
-    }, 3000); */
 }
