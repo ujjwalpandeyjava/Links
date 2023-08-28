08104a9c31f01dc8c713a5b5a80d4a3ed663c3f4
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2023-08-28 13:55:12 +0530

    Updates
    Added, updated and deleted example and changed UI on few places.

--- loadingExamples.js
+++ loadingExamples.js
@@ -7,23 +7,20 @@ function loadData() {
     var userMailID = "ujjwalPandey.aps@gmail.com"   //Find a way to change this name using this variable
     $.getJSON("Links_heads.json",
         function (jsonData) {
-            let user = jsonData.filter(data => { if (data.maild_id === "ujjwalpandey.aps@gmail.com") return data; })[0]
-            // console.log('we got the data ', user);   
-            //Data loaded in the variable "user" Now hide the loader
+            let user = jsonData.filter(data => { if (data.mail_id === "ujjwalpandey.aps@gmail.com") return data; });
             setTimeout(() => {
-                hideLoader();
-                //Adding links choose any one
-                addingLinksAtOnce(user);
-                // addingLinksAtInterval
-            }, 1150);
+                toggleLoader();
+                addingLinksAtOnce(user[0]);
+                // }, 800);
+            }, 10);
         }
     );
 }
 
 // Add links in loop
 function addingLinksAtOnce(userData) {
+    console.log(userData);
     userData.WorkExamples.forEach(singleData => {
-        // console.log(singleData);
         let dataToBeAdded = `
         <div class="links">
             <div>
@@ -39,18 +36,8 @@ function addingLinksAtOnce(userData) {
     });
     $(".addLinksBeforeMe").hide();
 }
-function addingLinksAtInterval(userData) {
-    //  with it the links will show one by one with a very minute interval of 200ms.
-    /*
-        Code writing
-        --> run a forEach loop on "userData.WorkExamples" and for every data in the loop will call function with the data at a setInterval or setTimeout in the loop,
-        the function called will do the all work of - loading json data -hiding loader - and adding it to the html.
-    */
-}
-
-
 
 // hide the Loading circle
-function hideLoader() {
+function toggleLoader() {
     $(".loader").toggle();
 }
